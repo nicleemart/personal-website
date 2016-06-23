7c671aa94f227c71b7fec07da4ec81783d6ec65d
Nikki <nicleemart@gmail.com>
2016-06-23 14:46:44 -0500

    Updated CSS

--- index.php
+++ index.php
@@ -9,10 +9,11 @@
 	<body>
 	<div class= "flex-container">
 		<div class="flex1">
-		<h1>Hello!</h1>
+		<h1>Nikki Martin</h1>
 		</div>
 	<div class="flex2">
 <?php include("navigation.php"); ?>
+	</div>
 	</div>
 			<p>My name is Nikki. I grew up in rural Southwestern Iowa and currently live in Omaha, Nebraska. I really enjoy traveling, especially abroad. In my free time, I like to cook, read, play video games and hang out with my dog.</p>
 	</body>
