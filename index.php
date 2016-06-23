be03a843da9e7d9ea6c189992b4e5fe62b6a15f5
Nikki <nicleemart@gmail.com>
2016-06-23 11:08:53 -0500

    Added Flexbox

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php $thisPage="Home"; ?>
 <?php $directory_prefix=""; ?>
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Nikki</title>
-		<!--<link href="style.css" type="text/css" rel="stylesheet" />-->
+<link href="style.css" type="text/css" rel="stylesheet" />
 	</head>
 	<body>
-	<div id="body">
+	<div class= "flex-container">
+	<div class="flex1">
 		<h1>Hello!</h1>
-	<p>My name is Nikki. I grew up in rural Southwestern Iowa and currently live in Omaha, Nebraska. I really enjoy traveling, especially abroad. In my free time, I like to cook, read, play video games and hang out with my dog.</p>
+			<p>My name is Nikki. I grew up in rural Southwestern Iowa and currently live in Omaha, Nebraska. I really enjoy traveling, especially abroad. In my free time, I like to cook, read, play video games and hang out with my dog.</p>
 	</div>
+	<div class="flex2">
 <?php include("navigation.php"); ?>
+	</div>
 	</body>
 </html>
