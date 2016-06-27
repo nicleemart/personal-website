f07717ca849428e4d8b3ccb4a2f091aed7ba20a9
Nikki <nicleemart@gmail.com>
2016-06-26 19:33:17 -0500

    Finished HTML Doc Tags page

--- articles/htmldoc.php
+++ articles/htmldoc.php
@@ -17,7 +17,10 @@
 			</div>
 			<div id="flex3">
 				<h2>HTML Doc Tags</h2>
-			<p></p>
+				<p>HTML has different tags that tell the browser how the process the content within the HTML document.<br><br>
+				The &lt;html&gt; tag lets the browser know that the document is HTML as to process it as such. It also serves as a container for all the other elements of the HTML document. All content will be between the two &lt;html&gt; tags.<br><br>
+				The &lt;head&gt; tag serves as a container for specific elements that belong in the head. These elements include fundamental tags such as the title of the webpage, &lt;title&gt;, and the CSS or design of the webpage, &lt;style&gt;. The meta tag, &lt;meta&gt;, also belongs here. Meta tags store data about data. By defining a char set in the meta tag, you are able to declare which characters you are using.<br><br>
+				The &lt;body&gt; is where all the content of the HTML document is located. In the body, you will find text in forms such as paragraphs or lists, images, links, or any other data type you are able to display through HTML. Therefore, the bulk of your work is done between the two &lt;body&gt; tags.</p>
 			</div>
 		</div>
 	</body>
